64203b2c5302c7a394473ba6423d25acdd4ebdc0
Steven Tsang <tsangste@gmail.com>
2023-09-27 10:59:43 +0100

    more command running fixes

--- renovate.config.js
+++ renovate.config.js
@@ -11,10 +11,12 @@ module.exports = {
   allowPostUpgradeCommandTemplating: true,
   allowedPostUpgradeCommands: [
     '^npm ci --ignore-scripts$',
+    '^npm install --ignore-scripts$',
     '^npm run prepare --if-present$',
     '^npm run format --if-present$',
     '^npx --no-install nx migrate (@[a-z0-9-~][a-z0-9-._~]*\\/)?[a-z0-9-~][a-z0-9-._~]* --from=(@[a-z0-9-~][a-z0-9-._~]*\\/)?[a-z0-9-~][a-z0-9-._~]*@\\d+\\.\\d+\\.\\d+ --to=(@[a-z0-9-~][a-z0-9-._~]*\\/)?[a-z0-9-~][a-z0-9-._~]*@\\d+\\.\\d+\\.\\d+$',
     '^\\[ -f migrations\\.json \\] && npx --no-install nx migrate --run-migrations=migrations\\.json$',
     '^rm -f migrations.json || true$',
+    '^npx --no-install nx run-many --target=lint --all --parallel --fix --skip-nx-cache$',
   ],
 }
