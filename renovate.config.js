d5fbe4becf7793660ce4532052f80468fbb26452
Steven Tsang <tsangste@gmail.com>
2023-09-28 14:48:56 +0100

    run-many `--all` option has been deprecated

--- renovate.config.js
+++ renovate.config.js
@@ -17,6 +17,6 @@ module.exports = {
     '^npx --no-install nx migrate (@[a-z0-9-~][a-z0-9-._~]*\\/)?[a-z0-9-~][a-z0-9-._~]* --from=(@[a-z0-9-~][a-z0-9-._~]*\\/)?[a-z0-9-~][a-z0-9-._~]*@\\d+\\.\\d+\\.\\d+ --to=(@[a-z0-9-~][a-z0-9-._~]*\\/)?[a-z0-9-~][a-z0-9-._~]*@\\d+\\.\\d+\\.\\d+$',
     '^\\[ -f migrations\\.json \\] && npx --no-install nx migrate --run-migrations=migrations\\.json$',
     '^rm -f migrations.json || true$',
-    '^npx --no-install nx run-many --target=lint --all --parallel --fix --skip-nx-cache$',
+    '^npx --no-install nx run-many --target=lint --parallel --fix --skip-nx-cache$',
   ],
 }
